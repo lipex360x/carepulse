d703b9b8a02221fc55f064e1fb0988258ead4766
Felipe Bambirra <lipex360@gmail.com>
2024-07-09 12:28:46 +0100

    setup finished

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,10 @@
+import { Button } from '@/components/ui/button'
+
 export default function Home() {
-  return <div>Home</div>
+  return (
+    <div>
+      <h1 className="underline text-3xl">Home</h1>
+      <Button>Click me</Button>
+    </div>
+  )
 }
